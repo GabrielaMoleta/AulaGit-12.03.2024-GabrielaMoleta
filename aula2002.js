b094f8261619d1104c9e56c4e690c5232236d606
Gabriela Moleta <gabrielamoleta.tds2.senai@gmail.com>
2024-03-12 16:54:15 -0300

    Mudei o valor da variavel let f

--- aula2002.js
+++ aula2002.js
@@ -1,7 +1,7 @@
 //Adicionei uma mensagem
 console.log("Seja Muito Bem Vindo a sua casa")
 //Declarei a variavel f
-let f = 75
+let f = 65
 let c = (f - 32 * 5 / 9);
 //Declarei um console de fahrenheit
 console.log(f.toFixed(1) + " fahrenheit");
