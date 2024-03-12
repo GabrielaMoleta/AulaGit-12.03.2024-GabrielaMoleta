e92f32322f0df82a847073322d3225f001ac92dc
Gabriela Moleta <gabrielamoleta.tds2.senai@gmail.com>
2024-03-12 16:35:00 -0300

    Adicionei um comentario para declarar variavel

--- aula2002.js
+++ aula2002.js
@@ -1,8 +1,8 @@
 //Adicionei uma mensagem
 console.log("Seja Bem Vindo a sua casa")
+//Declarei a variavel f
 let f = 75
 let c = (f - 32 * 5 / 9);
 console.log(f.toFixed(1) + " fahrenheit");
 console.log(c.toFixed(1) + " celsius");
 console.log("A temperatura é")
-console.log("calor")
